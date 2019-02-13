839ac93a121fb77dc1a2ccee65e8b71b4adf6ecd
Willie Jeng <willie.jeng@gmail.com>
2019-02-12 19:39:17 -0800

    Added announcement

--- announcement_list.js
+++ announcement_list.js
@@ -1,4 +1,5 @@
 // add new announcements to the top 
 // use html syntax
 var announcements = [
+	"<p><b>2/12/2019 - First meeting coming soon!</b></p>\n<p>Our first club meeting will be next Wednesday (2/20) at lunch in room 903. Be there for an introduction of linguistics!</p>\n"
 ];
