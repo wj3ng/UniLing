48183a5dab2ed8ba00058c17fe11fb7902a5c60c
Willie Jeng <willie.jeng@gmail.com>
2019-02-20 15:29:53 -0800

    Added 02/20/2019 lecture

--- lectures_list.js
+++ lectures_list.js
@@ -1,4 +1,5 @@
 // add new lectures to the top 
 // use html syntax
 var lectures = [
+	"<p><b>2/20/2019 - Introduction to Linguistics</b></p>\n<p>Thank you to everyone who came to our first club meeting! We discussed the subfields of linguistics and made an introduction to linguistic puzzles.<br\>Today's presentation can be found <a href=\"https://slides.com/wjeng/uniling01\">here</a>.</p>\n"
 ];
