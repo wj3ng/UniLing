6da34a997c473c346f8a90509d95255823f30c6e
Willie Jeng <willie.jeng@gmail.com>
2019-02-27 15:06:54 -0800

    Added lecture 02272019

--- lectures_list.js
+++ lectures_list.js
@@ -1,5 +1,7 @@
 // add new lectures to the top 
 // use html syntax
 var lectures = [
+	//TODO: nativlang video
+	"<p><b>2/27/2019 - The International Phonetic Alphabet</b></p>\n<p>Today's lecture was on the IPA, which served as an introduction to phonetics. Below are some resources used today.</p>\n<ul><li><a href=\"https://slides.com/wjeng/uniling02\">Todays's presentation</a></li><li><a href=\"http://www.ipachart.com/\">An interactive IPA chart</a></li><li><a href=\"https://www.youtube.com/watch?v=W6WO5XabD-s\">YouTube video of a man speaking in a language with clicks</a></li><li><a href=\"https://www.youtube.com/watch?v=-zB2TG9sVf8\">A nice video by NativLang on weird phonemes</a></li><li><a href=\"https://www.nacloweb.org/resources/problems/2012/N2012-H.pdf\">Waorani: a puzzle on number systems</a> (<a href=\"https://www.nacloweb.org/resources/problems/2012/N2012-HS.pdf\">solution</a>)</li></ul>",
 	"<p><b>2/20/2019 - Introduction to Linguistics</b></p>\n<p>Thank you to everyone who came to our first club meeting! We discussed the subfields of linguistics and made an introduction to linguistic puzzles.<br\>Today's presentation can be found <a href=\"https://slides.com/wjeng/uniling01\">here</a>.</p>\n"
 ];
